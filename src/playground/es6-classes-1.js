5e62fbeeb332d2e2f7f8046a550f95e78ce7340b
Julien Schmitt <julien.schmitt@imaios.com>
2018-04-26 10:39:21 +0200

    25

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,22 +1,80 @@
 class Person {
-    constructor(name = 'John Doe', age = 0){
+    constructor(name = 'John Doe', age = 0) {
         this.name = name;
         this.age = age;
     }
 
-    getGretting(){
+    getGreeting() {
         return `Hi ${this.name}!`;
     }
 
-    getDescription(){
-        return `${this.name} is ${this.age} ${this.age > 1 ? 'years': 'year'} old`;
+    getDescription() {
+        return `${this.name} is ${this.age} ${this.age > 1 ? 'years' : 'year'} old`;
     }
 }
 
-const me = new Person('Julien Schmitt', 30);
-console.log(me.getGretting());
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor(){
+        // !'' => true
+        // !!'' => false
+        // !!'Julien' => true
+        // !!undefined => false
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasMajor()){
+            description += ` and his major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if(this.hasHomeLocation()){
+            greeting += ` You are from ${this.homeLocation}`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student('Julien Schmitt', 30, 'Computer Science');
+console.log(me);
+console.log(me.getGreeting());
 console.log(me.getDescription());
 
-const anon = new Person();
-console.log(anon.getGretting());
+const anon = new Student();
+console.log(anon);
+console.log(anon.getGreeting());
 console.log(anon.getDescription());
+
+const her = new Traveler('Elaine Lam', 27, 'Hong-Kong');
+console.log(her);
+console.log(her.getGreeting());
+console.log(her.getDescription());
+
+const anonymous = new Traveler();
+console.log(anonymous);
+console.log(anonymous.getGreeting());
+console.log(anonymous.getDescription());
