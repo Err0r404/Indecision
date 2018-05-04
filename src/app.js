fb3eb0e0e986746da59f72728fbb97735b5c5b4e
Julien Schmitt <julien.schmitt@imaios.com>
2018-05-04 10:43:05 +0200

    64

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles/styles.scss';
+
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
